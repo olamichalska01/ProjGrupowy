b9ce659ee3ef3ff46da0bdc35b978c414a434636
Hubert2718 <hubert.buczynski123@gmail.com>
2023-05-20 18:03:43 +0200

    wyszukiwanie eventów backend

---
+++ ComUnity/src/ComUnity.Application/Features/ManagingEvents/SearchEvent.cs
@@ -0,0 +1,54 @@
+﻿using ComUnity.Application.Common;
+using ComUnity.Application.Database;
+using ComUnity.Application.Features.ManagingEvents.Dtos;
+using ComUnity.Application.Features.ManagingEvents.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ComUnity.Application.Features.ManagingEvents;
+
+public class SearchEvent : ApiControllerBase
+{
+    [HttpGet("/api/events/{searchText}")]
+    public async Task<ActionResult<GetEventsResponse>> GetEvents([FromRoute] string searchText)
+    {
+        return await Mediator.Send(new GetEventsQuery(searchText));
+    }
+
+    public record GetEventsQuery(string searchText) : IRequest<GetEventsResponse>;
+
+    public record GetEventsResponse(ICollection<EventDto> Events);
+
+    internal class GetEventByIdQueryHandler : IRequestHandler<GetEventsQuery, GetEventsResponse>
+    {
+        private readonly ComUnityContext _context;
+
+        public GetEventByIdQueryHandler(ComUnityContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<GetEventsResponse> Handle(GetEventsQuery request, CancellationToken cancellationToken)
+        {
+            var events = await _context.Set<Event>()
+                .Where(e => e.EventName.ToLower().Contains(request.searchText.ToLower())
+                ||
+                e.Place.ToLower().Contains(request.searchText.ToLower()))
+                .ToListAsync();
+
+            return new GetEventsResponse(
+                events.Select(e => new EventDto(
+                    e.Id,
+                    e.EventName,
+                    e.MaxAmountOfPeople,
+                    e.Place,
+                    e.EventDate,
+                    e.Cost,
+                    e.MinAge,
+                    e.EventCategory.CategoryName)).ToList());
+        }
+    }
+}
+
+
