e27cce830953b851d880202f90f14b9433ca5411
Michał Szcześniak <77015941+szczesniakm@users.noreply.github.com>
2023-05-21 22:42:25 +0200

    add api client generator (#4)

--- ComUnity.Frontend/src/ComUnity.Frontend/Client/Program.cs
+++ ComUnity.Frontend/src/ComUnity.Frontend/Client/Program.cs
@@ -1,4 +1,4 @@
-using ComUnity.Frontend;
+using ComUnity.Frontend.Api;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -12,7 +12,13 @@ namespace ComUnity.Frontend
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped<IComUnityApiClient>(sp =>
+            {
+                var httpClient = new HttpClient { BaseAddress = new Uri("https://localhost:7229/") };
+                var client = new ComUnityApiClient(httpClient);
+
+                return client;
+            });
 
             await builder.Build().RunAsync();
         }
