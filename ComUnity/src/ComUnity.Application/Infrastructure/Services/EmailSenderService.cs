52215687303f19c193f8e8779c55ede4dba96ec8
Michał Szcześniak <77015941+szczesniakm@users.noreply.github.com>
2023-06-04 16:58:30 +0200

    add sending email verification link (#8)

---
+++ ComUnity/src/ComUnity.Application/Infrastructure/Services/EmailSenderService.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.Mail;
+using System.Net;
+using ComUnity.Application.Infrastructure.Settings;
+using Microsoft.Extensions.Options;
+
+namespace ComUnity.Application.Infrastructure.Services;
+
+public class EmailSenderService : IEmailSenderService
+{
+    private readonly SmtpSettings _smtpSettings;
+
+    public EmailSenderService(IOptions<SmtpSettings> smtpSettings)
+    {
+        _smtpSettings = smtpSettings.Value;
+    }
+
+    public async Task SendEmail(MailMessage message)
+    {
+        var smtpClient = new SmtpClient(_smtpSettings.Host)
+        {
+            Port = _smtpSettings.Port,
+            Credentials = new NetworkCredential(_smtpSettings.Account, _smtpSettings.Password),
+            EnableSsl = _smtpSettings.EnableSsl,
+        };
+
+        await smtpClient.SendMailAsync(message);
+    }
+}
+
+public interface IEmailSenderService
+{
+    Task SendEmail(MailMessage message);
+}
