e9350b89f97a9ad1b5a7b5d24263c81d25e16e48
Mateusz Baran <mateusz.baran3008@onet.pl>
2023-06-08 20:02:21 +0200

    added event fomr

--- ComUnity.Frontend/src/ComUnity.Frontend/Api/ComUnityApiClient.cs
+++ ComUnity.Frontend/src/ComUnity.Frontend/Api/ComUnityApiClient.cs
@@ -4981,6 +4981,9 @@ namespace ComUnity.Frontend.Api
     [System.CodeDom.Compiler.GeneratedCode("NJsonSchema", "13.19.0.0 (NJsonSchema v10.9.0.0 (Newtonsoft.Json v13.0.0.0))")]
     public partial class AddEventCategoryCommand
     {
+        [Newtonsoft.Json.JsonProperty("id", Required = Newtonsoft.Json.Required.DisallowNull, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
+        public System.Guid Id { get; set; }
+
         [Newtonsoft.Json.JsonProperty("name", Required = Newtonsoft.Json.Required.Default, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
         public string Name { get; set; }
 
@@ -5212,6 +5215,9 @@ namespace ComUnity.Frontend.Api
     [System.CodeDom.Compiler.GeneratedCode("NJsonSchema", "13.19.0.0 (NJsonSchema v10.9.0.0 (Newtonsoft.Json v13.0.0.0))")]
     public partial class EventCategory
     {
+        [Newtonsoft.Json.JsonProperty("id", Required = Newtonsoft.Json.Required.DisallowNull, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
+        public System.Guid Id { get; set; }
+
         [Newtonsoft.Json.JsonProperty("name", Required = Newtonsoft.Json.Required.Default, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
         public string Name { get; set; }
 
