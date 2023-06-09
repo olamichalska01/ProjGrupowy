6b7493eada3447c7af665482f6fd7d927cd3ea70
Hubert2718 <hubert.buczynski123@gmail.com>
2023-06-09 22:49:49 +0200

    Deleting Friends

--- ComUnity.Frontend/src/ComUnity.Frontend/Api/ComUnityApiClient.cs
+++ ComUnity.Frontend/src/ComUnity.Frontend/Api/ComUnityApiClient.cs
@@ -113,6 +113,15 @@ namespace ComUnity.Frontend.Api
 
         /// <returns>No Content</returns>
         /// <exception cref="SwaggerException">A server side error occurred.</exception>
+        System.Threading.Tasks.Task ApiUsersDeleteFriendAsync(System.Guid userId);
+
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        /// <returns>No Content</returns>
+        /// <exception cref="SwaggerException">A server side error occurred.</exception>
+        System.Threading.Tasks.Task ApiUsersDeleteFriendAsync(System.Guid userId, System.Threading.CancellationToken cancellationToken);
+
+        /// <returns>No Content</returns>
+        /// <exception cref="SwaggerException">A server side error occurred.</exception>
         System.Threading.Tasks.Task ApiFavoriteAsync(DeleteFavoriteCategoryCommand body);
 
         /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
@@ -1562,6 +1571,138 @@ namespace ComUnity.Frontend.Api
             var urlBuilder_ = new System.Text.StringBuilder();
             urlBuilder_.Append("api/event-categories/{name}");
             urlBuilder_.Replace("{name}", System.Uri.EscapeDataString(ConvertToString(name, System.Globalization.CultureInfo.InvariantCulture)));
+
+            var client_ = _httpClient;
+            var disposeClient_ = false;
+            try
+            {
+                using (var request_ = new System.Net.Http.HttpRequestMessage())
+                {
+                    request_.Method = new System.Net.Http.HttpMethod("DELETE");
+
+                    PrepareRequest(client_, request_, urlBuilder_);
+
+                    var url_ = urlBuilder_.ToString();
+                    request_.RequestUri = new System.Uri(url_, System.UriKind.RelativeOrAbsolute);
+
+                    PrepareRequest(client_, request_, url_);
+
+                    var response_ = await client_.SendAsync(request_, System.Net.Http.HttpCompletionOption.ResponseHeadersRead, cancellationToken).ConfigureAwait(false);
+                    var disposeResponse_ = true;
+                    try
+                    {
+                        var headers_ = System.Linq.Enumerable.ToDictionary(response_.Headers, h_ => h_.Key, h_ => h_.Value);
+                        if (response_.Content != null && response_.Content.Headers != null)
+                        {
+                            foreach (var item_ in response_.Content.Headers)
+                                headers_[item_.Key] = item_.Value;
+                        }
+
+                        ProcessResponse(client_, response_);
+
+                        var status_ = (int)response_.StatusCode;
+                        if (status_ == 400)
+                        {
+                            var objectResponse_ = await ReadObjectResponseAsync<ValidationProblemDetails>(response_, headers_, cancellationToken).ConfigureAwait(false);
+                            if (objectResponse_.Object == null)
+                            {
+                                throw new SwaggerException("Response was null which was not expected.", status_, objectResponse_.Text, headers_, null);
+                            }
+                            throw new SwaggerException<ValidationProblemDetails>("Bad Request", status_, objectResponse_.Text, headers_, objectResponse_.Object, null);
+                        }
+                        else
+                        if (status_ == 401)
+                        {
+                            var objectResponse_ = await ReadObjectResponseAsync<ProblemDetails>(response_, headers_, cancellationToken).ConfigureAwait(false);
+                            if (objectResponse_.Object == null)
+                            {
+                                throw new SwaggerException("Response was null which was not expected.", status_, objectResponse_.Text, headers_, null);
+                            }
+                            throw new SwaggerException<ProblemDetails>("Unauthorized", status_, objectResponse_.Text, headers_, objectResponse_.Object, null);
+                        }
+                        else
+                        if (status_ == 403)
+                        {
+                            var objectResponse_ = await ReadObjectResponseAsync<ProblemDetails>(response_, headers_, cancellationToken).ConfigureAwait(false);
+                            if (objectResponse_.Object == null)
+                            {
+                                throw new SwaggerException("Response was null which was not expected.", status_, objectResponse_.Text, headers_, null);
+                            }
+                            throw new SwaggerException<ProblemDetails>("Forbidden", status_, objectResponse_.Text, headers_, objectResponse_.Object, null);
+                        }
+                        else
+                        if (status_ == 404)
+                        {
+                            var objectResponse_ = await ReadObjectResponseAsync<ProblemDetails>(response_, headers_, cancellationToken).ConfigureAwait(false);
+                            if (objectResponse_.Object == null)
+                            {
+                                throw new SwaggerException("Response was null which was not expected.", status_, objectResponse_.Text, headers_, null);
+                            }
+                            throw new SwaggerException<ProblemDetails>("Not Found", status_, objectResponse_.Text, headers_, objectResponse_.Object, null);
+                        }
+                        else
+                        if (status_ == 422)
+                        {
+                            var objectResponse_ = await ReadObjectResponseAsync<ProblemDetails>(response_, headers_, cancellationToken).ConfigureAwait(false);
+                            if (objectResponse_.Object == null)
+                            {
+                                throw new SwaggerException("Response was null which was not expected.", status_, objectResponse_.Text, headers_, null);
+                            }
+                            throw new SwaggerException<ProblemDetails>("Client Error", status_, objectResponse_.Text, headers_, objectResponse_.Object, null);
+                        }
+                        else
+                        if (status_ == 500)
+                        {
+                            var objectResponse_ = await ReadObjectResponseAsync<ProblemDetails>(response_, headers_, cancellationToken).ConfigureAwait(false);
+                            if (objectResponse_.Object == null)
+                            {
+                                throw new SwaggerException("Response was null which was not expected.", status_, objectResponse_.Text, headers_, null);
+                            }
+                            throw new SwaggerException<ProblemDetails>("Server Error", status_, objectResponse_.Text, headers_, objectResponse_.Object, null);
+                        }
+                        else
+                        if (status_ == 204)
+                        {
+                            return;
+                        }
+                        else
+                        {
+                            var responseData_ = response_.Content == null ? null : await response_.Content.ReadAsStringAsync().ConfigureAwait(false);
+                            throw new SwaggerException("The HTTP status code of the response was not expected (" + status_ + ").", status_, responseData_, headers_, null);
+                        }
+                    }
+                    finally
+                    {
+                        if (disposeResponse_)
+                            response_.Dispose();
+                    }
+                }
+            }
+            finally
+            {
+                if (disposeClient_)
+                    client_.Dispose();
+            }
+        }
+
+        /// <returns>No Content</returns>
+        /// <exception cref="SwaggerException">A server side error occurred.</exception>
+        public virtual System.Threading.Tasks.Task ApiUsersDeleteFriendAsync(System.Guid userId)
+        {
+            return ApiUsersDeleteFriendAsync(userId, System.Threading.CancellationToken.None);
+        }
+
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        /// <returns>No Content</returns>
+        /// <exception cref="SwaggerException">A server side error occurred.</exception>
+        public virtual async System.Threading.Tasks.Task ApiUsersDeleteFriendAsync(System.Guid userId, System.Threading.CancellationToken cancellationToken)
+        {
+            if (userId == null)
+                throw new System.ArgumentNullException("userId");
+
+            var urlBuilder_ = new System.Text.StringBuilder();
+            urlBuilder_.Append("api/users/{userId}/delete-friend");
+            urlBuilder_.Replace("{userId}", System.Uri.EscapeDataString(ConvertToString(userId, System.Globalization.CultureInfo.InvariantCulture)));
 
             var client_ = _httpClient;
             var disposeClient_ = false;
---
+++ ComUnity/src/ComUnity.Application/Features/UserProfileManagement/DeleteFriend.cs
@@ -0,0 +1,85 @@
+﻿using ComUnity.Application.Common;
+using ComUnity.Application.Database;
+using ComUnity.Application.Features.UserProfileManagement.Entities;
+using ComUnity.Application.Features.UserProfileManagement.Exceptions;
+using ComUnity.Application.Infrastructure.Services;
+using MassTransit;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ComUnity.Application.Features.UserProfileManagement;
+
+public class DeleteFriendController : ApiControllerBase
+{
+    [HttpDelete("/api/users/{userId}/delete-friend")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public async Task<IActionResult> DeleteFriend([FromRoute] Guid userId, CancellationToken cancellationToken)
+    {
+        await Mediator.Send(new DeleteFriendCommand(userId), cancellationToken);
+
+        return NoContent();
+    }
+}
+
+public record DeleteFriendCommand(Guid UserId) : IRequest<Unit>;
+
+internal class DeleteFriendCommandHandler : IRequestHandler<DeleteFriendCommand, Unit>
+{
+    private readonly ComUnityContext _context;
+    private readonly IAuthenticatedUserProvider _authenticatedUserProvider;
+
+    public DeleteFriendCommandHandler(ComUnityContext context, IAuthenticatedUserProvider authenticatedUserProvider)
+    {
+        _context = context;
+        _authenticatedUserProvider = authenticatedUserProvider;
+    }
+
+    public async Task<Unit> Handle(DeleteFriendCommand request, CancellationToken cancellationToken)
+    {
+        var sender = await _authenticatedUserProvider.GetUserProfile(cancellationToken);
+        var receiverExists = await _context.Set<UserProfile>().AnyAsync(x => x.UserId == request.UserId, cancellationToken);
+
+        if (!receiverExists)
+        {
+            throw new UserDoesNotExistException(request.UserId);
+        }
+
+
+        var relationship1 = await _context.Set<Relationship>().FirstOrDefaultAsync(x =>
+            (x.User2Id == request.UserId && x.User1Id == sender.UserId),
+            cancellationToken);
+        var relationship2 = await _context.Set<Relationship>().FirstOrDefaultAsync(x =>
+            (x.User1Id == request.UserId && x.User2Id == sender.UserId),
+            cancellationToken);
+
+        if ((relationship1 is null) || (relationship2 is null))
+        {
+
+            throw new FriendshipDoesNotExistsException();
+        }
+
+        _context.Remove(relationship1);
+        _context.Remove(relationship2);
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return Unit.Value;
+    }
+}
+
+public class DeleteFriendEvent : DomainEvent
+{
+    public string SenderName { get; }
+
+    public Guid RequestId { get; }
+
+    public Guid ReceiverId { get; }
+
+    public DeleteFriendEvent(string senderName, Guid requestId, Guid receiverId)
+    {
+        SenderName = senderName;
+        RequestId = requestId;
+        ReceiverId = receiverId;
+    }
+}
